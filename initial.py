64a1b24dc1556738a9b50e51510bb22d50d126ca
hhhhojeihsu(Workstation) <hhhhojeihsu@gmail.com>
2019-05-20 17:54:43 +0800

    Update Function Prototype

--- initial.py
+++ initial.py
@@ -1,3 +1,38 @@
+class Population:
+    """Class of population that includes all the individuals
+
+    Attributes:
+        connection_matrix (2d list): The connection matrix that represents the network. Must be square matrix.
+        source (int): The index(zero-based numbering) indicate the starting point of the path.
+        dest (int): The index(zero-based numbering) indicate the ending point of the path.
+        iddfs_depth (int): The maximum depth of IDDFS.
+        population_size (int): How many individuals should the function return.
+        max_individual_size (int): The path should not be larger than this value.
+    """
+
+    def __init__(self, connection_matrix, source, dest, iddfs_depth, population_size, max_individual_size):
+        self.connection_matrix = connection_matrix
+        self.source = source
+        self.dest = dest
+        self.iddfs_depth = iddfs_depth
+        self.population_size = population_size
+        self.max_individual_size = max_individual_size
+        self.visited_nodes = []
+
+        # Start iddfs, done in Popultaion. Becasue all individuals share it
+        # Reference: https://www.geeksforgeeks.org/iterative-deepening-searchids-iterative-deepening-depth-first-searchiddfs/
+        self.IDDFS()
+
+        # Populate each individaul
+        for i in range(self.population_size):
+            Individual(connection_matrix, source, dest, max_individual_size)
+
+    def IDDFS(self):
+        pass
+
+    def DLS(self):
+        pass
+
 
 class Individual:
     """Class of individual that initialize using run iddfs and random search
@@ -6,35 +41,23 @@ class Individual:
         connection_matrix (2d list): The connection matrix that represents the network. Must be square matrix.
         source (int): The index(zero-based numbering) indicate the starting point of the path.
         dest (int): The index(zero-based numbering) indicate the ending point of the path.
-        iddfs_depth (int): The maximum depth of IDDFS.
         max_individual_size (int): The path should not be larger than this value.
     """
 
-    def __init__(self, connection_matrix, source, dest, iddfs_depth, max_individual_size):
+    def __init__(self, connection_matrix, source, dest, max_individual_size):
         # Set variables
         self.source = source
         self.dest = dest
-        self.iddfs_depth = iddfs_depth
         self.max_individual_size = max_individual_size
-        self.visited_nodes = []
 
-        # Start iddfs
-        # Reference: https://www.geeksforgeeks.org/iterative-deepening-searchids-iterative-deepening-depth-first-searchiddfs/
-        self.IDDFS()
-
-    def IDDFS(self):
-        pass
-
-    def DLS(self):
+    def random_search(self):
         pass
 
 def initial(connection_matrix, source, dest, iddfs_depth, population_size, max_individual_size):
     """Initialize the population with given network and return in 2d list
 
     The path return will not contain cycle
-
-    Args:
-        connection_matrix (2d list): The connection matrix that represents the network. Must be square matrix.
+Args: connection_matrix (2d list): The connection matrix that represents the network. Must be square matrix.
         source (int): The index(zero-based numbering) indicate the starting point of the path.
         dest (int): The index(zero-based numbering) indicate the ending point of the path.
         iddfs_depth (int): The maximum depth of IDDFS.
@@ -45,9 +68,6 @@ def initial(connection_matrix, source, dest, iddfs_depth, population_size, max_i
         2d list: Each element is a variable lengthed path.
 
     """
-    result = []
 
-    for i in range(population_size):
-        result.append(Individual(connection_matrix, source, dest, iddfs_depth, max_individual_size))
-    return result
+    return Population(connection_matrix, source, dest, iddfs_depth, population_size, max_individual_size)
 
