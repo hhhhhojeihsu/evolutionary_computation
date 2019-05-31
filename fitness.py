9c6ab3395427e8c02f6e994caf383ce89c745bc7
Hojei Hsu(PC) <hhhhojeihsu@gmail.com>
2019-05-31 16:21:19 +0800

    Fix Array OOB In fitness.py

--- fitness.py
+++ fitness.py
@@ -5,7 +5,7 @@ def fitness(individual, connection_matrix):
         individual (1d list): list of nodes in an individual
         connection_matrix (2d list): The connection matrix that represents the network (square matrix)
     '''
-    return min(connection_matrix[individual[i]][individual[i+1]] for i in range(len(individual)-1))
+    return min(connection_matrix[individual[i]-1][individual[i+1]-1] for i in range(len(individual)-1))
 
 def population_with_fitness(population, connection_matrix):
     '''
