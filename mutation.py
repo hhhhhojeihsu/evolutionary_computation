ad1116c2579031742c85d746515adb2e82da3e49
Wilbert <wilbert.phen@gmail.com>
2019-06-04 16:12:03 +0800

    Fixing problem in random walk

--- mutation.py
+++ mutation.py
@@ -1,17 +1,19 @@
 import random
 
-def mutation(neighbor_matrix, ind):
+def mutation(neighbor_matrix, ind, rate=0.1):
     # supposed individual is a list
     if len(ind) <= 2:
         pass
     else:
-        position = random.randint(1, len(ind)-2)
-        prev_pos = ind[position-1]
-        next_pos = ind[position+1]
-        intersection = list(set(neighbor_matrix[prev_pos]) & set(neighbor_matrix[next_pos]))
-
-        # +1 is for matching the vertex number from 1 to N (not from 0)
-        ind[position] = random.sample(intersection, 1)[0]
+        rate = max(0.9, rate)
+        freq = max(1, int(rate * len(ind)))
+        for i in range(freq):
+            
+            position = random.randint(1, len(ind)-2)
+            prev_pos = ind[position-1]
+            next_pos = ind[position+1]
+            intersection = list(set(neighbor_matrix[prev_pos]) & set(neighbor_matrix[next_pos]))
+            ind[position] = random.sample(intersection, 1)[0]
 
     return ind
 
